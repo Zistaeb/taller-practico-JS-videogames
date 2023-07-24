f3ddd8b97383ee01b1bebc808310489359ead116
Zistaeb <bperezatencio@gmail.com>
2023-07-24 15:33:02 -0300

    Tamaño Canvas y sus elementos

--- game.js
+++ game.js
@@ -4,11 +4,37 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame () {
+    let canvasSize;
+
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    };
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = (canvasSize/10) - 1;
+
+    console.log({canvasSize, elementsSize});
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = '';
+
+    for(let i = 1; i <=10; i++ ) {
+        game.fillText(emojis['X'], elementsSize, elementsSize * i);
+    };
+
+    
+     //window.innerHeight
+     //window.innerWidth
+
     //game.fillRect(0,0,100,100);
     //game.clearRect(0,0,50,100);
 
-    game.font = '25px Verdana';
-    game.fillStyle = 'purple';
-    game.textAlign = 'left';                  //start, end, center, rigth, left
-    game.fillText('Platzi', 25, 25);
+    //game.font = '25px Verdana';
+    //game.fillStyle = 'purple';
+    //game.textAlign = 'left';                  //start, end, center, rigth, left
+    //game.fillText('Platzi', 25, 25);
 };

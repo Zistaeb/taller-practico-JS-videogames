0eb5224e4309587881b32788f35fb9fe208e1aa5
Betty <bperezatencio@gmail.com>
2023-07-25 23:04:54 -0300

    Array multidimensionales JS

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express');
-var fs = require('fs');
-var https = require('https');
 
 const app = express();
 
 app.use(express.static("public"));
-
-app.get('/', (req, res) => {
-    res.send('Hola estas en la pagina principal');  
-    console.log('Se recibio una peticion get')
-});
 
 const port = 80;
 app.listen(port, () => {
